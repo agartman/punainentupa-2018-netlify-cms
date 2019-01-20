9b095878d732fd7962a2ebf073bb07e6a0f51145
agartman <atte.gartman@gmail.com>
2019-01-20 20:25:47 +0200

    Favicon and better contrast, app manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,39 @@ module.exports = {
       options: {
         plugins: []
       }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/img/favicon.png",
+  
+        // WebApp Manifest Configuration
+        appName: 'Punainen Tupa Solutions Oy website', // Inferred with your package.json
+        appDescription: null,
+        developerName: 'Atte Gartman',
+        developerURL: null,
+        dir: 'auto',
+        lang: 'fi',
+        background: '#fff',
+        theme_color: '#ce2d2d',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/',
+        version: '1.0',
+  
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
     }
   ]
 };
