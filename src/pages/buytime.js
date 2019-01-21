56f9fb06fe2067d9eae50d2873f21761ccda9a04
agartman <atte.gartman@gmail.com>
2019-01-21 16:21:24 +0200

    Styling and prod config

--- src/pages/buytime.js
+++ src/pages/buytime.js
@@ -1,49 +1,57 @@
 import React from "react";
 import Layout from "../layouts";
 import Content from "../components/Content";
+import "../css/shop.scss";
+import LeftArrow from "../img/left.png";
+import Link from "gatsby-link";
 
 export default () => {
   return (
     <Layout language="en" shownav={false} shop={true}>
-    
       <Content
         className={`section angled hero bg-red blog-hero`}
-        content={"Buy time"}
+        content={"Osta konsultointia"}
       />
-      <div className="blog-post">
-        <h3>Tarvitsetko lisäkäsiä heti?</h3>
+      <div className="shop">
         <p>
-          Kiirettä tarjouksen kanssa? Haluaisitko arvion saamastasi
-          tarjouksesta? Tilaa muutama tunti niin katsotaan. Ei jäykkiä
-          toimeksiantosopimuksia.
+          Kiirettä tarjouksien tekemisen/arviointien kanssa? Voin varmasti
+          auttaa. Haluaisitko arkkitehtuurikuvauksen nykytilastasi tai muuten
+          vaan suuntaa antavaa neuvoa?
         </p>
-        <strong>110€ + VAT</strong>
-        <p>
-          <a
-            style={{
-              backgroundColor: "#212121",
-              borderRadius: "5px",
-              color: "#F5F5F5",
-              fontWeight: "bold",
-              paddingBottom: "15px",
-              paddingTop: "15px",
-              paddingRight: "35px",
-              paddingLeft: "35px",
-              fontSize: "24"
-            }}
-            id="buyButton"
-            href="#"
-            className="snipcart-add-item buyBtn"
-            data-item-id={"time1h"}
-            data-item-price={110}
-            data-item-image={'https://thirsty-mcnulty-4b6910.netlify.com/img/punainentupalogo.svg'}
-            data-item-name={"1h aikaa"}
-            data-item-description={"1h of work"}
-            data-item-url={"https://thirsty-mcnulty-4b6910.netlify.com/buytime"}
-          >
-            Lisää 1h koriin
-          </a>
-        </p>
+        <div className="shop-controls">
+          <p>
+            <strong>Tuntihinta 110€</strong> + alv
+          </p>
+          <p>
+            <a
+              id="buyButton"
+              href="#"
+              className="snipcart-add-item btn btn-1"
+              data-item-id={"time1h"}
+              data-item-price={110}
+              data-item-image={
+                "https://punainentupa.fi/img/punainentupalogo.svg"
+              }
+              data-item-name={"1h aikaa"}
+              data-item-description={"1h of work"}
+              data-item-custom1-name={"Viesti"}
+              data-item-custom1-value={""}
+              data-item-url={
+                "https://punainentupa.fi/buytime"
+              }
+            >
+              Lisää 1h koriin
+            </a>
+          </p>
+          <p>
+            Voit tarvittaessa lähettää Skype-linkin, NDA:t tai muut
+            yksityiskohdat sähköpostilla meiliin{" "}
+            <a href="mailto:atte@punainentupa.fi">atte@punainentupa.fi</a>
+          </p>
+        </div>
+        <Link to="/">
+          <img src={LeftArrow} className="arrow" />
+        </Link>
       </div>
     </Layout>
   );
