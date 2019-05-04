d60442ed31f1aae52d5af329decc5cf4538e9a45
Atte Gartman <atte.gartman@gmail.com>
2019-05-04 20:35:27 +0300

    Remove sourcemaps

--- gatsby-node.js
+++ gatsby-node.js
@@ -63,3 +63,13 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     });
   }
 };
+
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  // If production JavaScript and CSS build
+  if (stage === 'build-javascript') {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+};
