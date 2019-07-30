29cea54f6555c894a16de9880ad5c4d90475c397
Atte Gartman <atte.gartman@gmail.com>
2019-07-30 11:40:49 +0300

    Set launchtimeout and also build the site for tests

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,9 @@
 // jest-puppeteer.config.js
 module.exports = {
   server: {
-    command: "./node_modules/.bin/gatsby serve",
-    port: 9000
+    command:
+      "./node_modules/.bin/gatsby build && ./node_modules/.bin/gatsby serve",
+    port: 9000,
+    launchTimeout: 10000
   }
 };
